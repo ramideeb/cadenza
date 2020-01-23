4f6c4045a28feabc9a988317168e5458ad9ca208
ramideeb <35407681+ramideeb@users.noreply.github.com>
2020-01-23 18:54:20 +0200

    audio player testing
    the first step is always the hardest one !! so keep going !!

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,247 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:audioplayer/audioplayer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+typedef void OnError(Exception exception);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+const kUrl = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3";
+const kUrl2 = "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3";
+
+void main() {
+  runApp(new MaterialApp(home: new Scaffold(body: new AudioApp())));
+}
+
+enum PlayerState { stopped, playing, paused }
+
+class AudioApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _AudioAppState createState() => new _AudioAppState();
+}
+
+class _AudioAppState extends State<AudioApp> {
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  String localFilePath;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initAudioPlayer();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void dispose() {
+    _positionSubscription.cancel();
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged
+        .listen((p) => setState(() => position = p));
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+      if (s == AudioPlayerState.PLAYING) {
+        setState(() => duration = audioPlayer.duration);
+      } else if (s == AudioPlayerState.STOPPED) {
+        onComplete();
+        setState(() {
+          position = duration;
+        });
+      }
+    }, onError: (msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
     });
   }
 
+  Future play() async {
+    await audioPlayer.play(kUrl);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future _playLocal() async {
+    await audioPlayer.play(localFilePath, isLocal: true);
+    setState(() => playerState = PlayerState.playing);
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = new Duration();
+    });
+  }
+
+  Future mute(bool muted) async {
+    await audioPlayer.mute(muted);
+    setState(() {
+      isMuted = muted;
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  Future<Uint8List> _loadFileBytes(String url, {OnError onError}) async {
+    Uint8List bytes;
+    try {
+      bytes = await readBytes(url);
+    } on ClientException {
+      rethrow;
+    }
+    return bytes;
+  }
+
+  Future _loadFile() async {
+    final bytes = await _loadFileBytes(kUrl,
+        onError: (Exception exception) =>
+            print('_loadFile => exception $exception'));
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = new File('${dir.path}/audio.mp3');
+
+    await file.writeAsBytes(bytes);
+    if (await file.exists())
+      setState(() {
+        localFilePath = file.path;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return new Center(
+        child: new Material(
+            elevation: 2.0,
+            color: Colors.grey[200],
+            child: new Center(
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    new Material(child: _buildPlayer()),
+                    localFilePath != null
+                        ? new Text(localFilePath)
+                        : new Container(),
+                    new Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            new RaisedButton(
+                              onPressed: () => _loadFile(),
+                              child: new Text('Download'),
+                            ),
+                            new RaisedButton(
+                              onPressed: () => _playLocal(),
+                              child: new Text('play local'),
+                            ),
+                          ]),
+                    )
+                  ]),
+            )));
+  }
+
+  Widget _buildPlayer() => new Container(
+      padding: new EdgeInsets.all(16.0),
+      child: new Column(mainAxisSize: MainAxisSize.min, children: [
+        new Row(mainAxisSize: MainAxisSize.min, children: [
+          new IconButton(
+              onPressed: isPlaying ? null : () => play(),
+              iconSize: 64.0,
+              icon: new Icon(Icons.play_arrow),
+              color: Colors.cyan),
+          new IconButton(
+              onPressed: isPlaying ? () => pause() : null,
+              iconSize: 64.0,
+              icon: new Icon(Icons.pause),
+              color: Colors.cyan),
+          new IconButton(
+              onPressed: isPlaying || isPaused ? () => stop() : null,
+              iconSize: 64.0,
+              icon: new Icon(Icons.stop),
+              color: Colors.cyan),
+        ]),
+        duration == null
+            ? new Container()
+            : new Slider(
+                value: position?.inMilliseconds?.toDouble() ?? 0.0,
+                onChanged: (double value) =>
+                    audioPlayer.seek((value / 1000).roundToDouble()),
+                min: 0.0,
+                max: duration.inMilliseconds.toDouble()),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            new IconButton(
+                onPressed: () => mute(true),
+                icon: new Icon(Icons.headset_off),
+                color: Colors.cyan),
+            new IconButton(
+                onPressed: () => mute(false),
+                icon: new Icon(Icons.headset),
+                color: Colors.cyan),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        new Row(mainAxisSize: MainAxisSize.min, children: [
+          new Padding(
+              padding: new EdgeInsets.all(12.0),
+              child: new Stack(children: [
+                new CircularProgressIndicator(
+                    value: 1.0,
+                    valueColor: new AlwaysStoppedAnimation(Colors.grey[300])),
+                new CircularProgressIndicator(
+                  value: position != null && position.inMilliseconds > 0
+                      ? (position?.inMilliseconds?.toDouble() ?? 0.0) /
+                          (duration?.inMilliseconds?.toDouble() ?? 0.0)
+                      : 0.0,
+                  valueColor: new AlwaysStoppedAnimation(Colors.cyan),
+                  backgroundColor: Colors.yellow,
+                ),
+              ])),
+          new Text(
+              position != null
+                  ? "${positionText ?? ''} / ${durationText ?? ''}"
+                  : duration != null ? durationText : '',
+              style: new TextStyle(fontSize: 24.0))
+        ])
+      ]));
 }
