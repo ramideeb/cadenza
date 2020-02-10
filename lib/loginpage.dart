5646b7376934debf17ec9feea85594be143cf46b
YousefAslan <YousefAslan9@gmail.com>
2020-02-10 20:48:44 +0200

    Adjust the screen ratio to fit the rest of the devices

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'SizeConfig.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -9,6 +10,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _usernameController;
   TextEditingController _passwordController;
   bool typing = false;
+  SizeConfig sizeConfig = new SizeConfig();
 
   @override
   void initState() {
@@ -28,19 +30,25 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    sizeConfig.init(context);
     Widget _logoRow = Container(
-      padding: EdgeInsets.only(left: 15, top: 30),
+      padding: EdgeInsets.only(
+          left: SizeConfig.blockSizeHorizontal * 3,
+          top: SizeConfig.blockSizeVertical * 3.5),
       child: Row(
         children: <Widget>[
           Image.asset("logo.png"),
           Padding(
-            padding: EdgeInsets.only(left: 15, bottom: 5),
+            padding: EdgeInsets.only(
+                left: SizeConfig.blockSizeHorizontal * 3,
+                bottom: SizeConfig.blockSizeVertical),
             child: Text(
               "Cadenza",
               style: TextStyle(
-                  fontSize: 25.0,
-                  color: Color.fromARGB(255, 230, 57, 70),
-                  fontFamily: "OpenSans"),
+                fontSize: 25.0,
+                color: Color.fromARGB(255, 230, 57, 70),
+                fontFamily: "OpenSans",
+              ),
             ),
           )
         ],
@@ -52,9 +60,9 @@ class _LoginPageState extends State<LoginPage> {
       // crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.only(left: 10,bottom: 80),
+          padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizontal*2, bottom: SizeConfig.blockSizeVertical*9.5),
           child: Stack(
-          overflow: Overflow.visible,
+            overflow: Overflow.visible,
             children: <Widget>[
               Positioned(
                 child: Text(
@@ -67,7 +75,8 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               Positioned(
-                top:20,
+                top: SizeConfig.blockSizeVertical*2.7,
+                left: SizeConfig.blockSizeHorizontal*-1,
                 child: Text(
                   "Rhythm",
                   style: TextStyle(
@@ -78,7 +87,7 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               Positioned(
-                top:87,
+                top: SizeConfig.blockSizeVertical*13,
                 child: Text(
                   "Of your heart",
                   style: TextStyle(
@@ -99,7 +108,7 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     Widget _inputRow = Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal*4),
       child: Column(
         children: <Widget>[
           Padding(
@@ -116,7 +125,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(bottom: 20),
+            padding: EdgeInsets.only(bottom: SizeConfig.blockSizeHorizontal*4),
             child: TextField(
               obscureText: true,
               controller: _passwordController,
@@ -132,8 +141,8 @@ class _LoginPageState extends State<LoginPage> {
           RaisedButton(
             onPressed: () {},
             child: Container(
-              width: 150,
-              height: 50,
+              width: SizeConfig.blockSizeHorizontal*40,
+              height: SizeConfig.blockSizeVertical*6,
               child: Center(
                 child: Text(
                   "Sing in",
@@ -146,7 +155,7 @@ class _LoginPageState extends State<LoginPage> {
             color: Color.fromARGB(255, 230, 57, 70),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*1.5),
             child: Text(
               "RESET YOUR PASSWORD >",
               style: TextStyle(
@@ -164,7 +173,7 @@ class _LoginPageState extends State<LoginPage> {
       fit: StackFit.expand,
       overflow: Overflow.clip,
       children: <Widget>[
-        Image.asset("BG.jpg", repeat: ImageRepeat.repeat),
+//        Image.asset("BG.jpg", repeat: ImageRepeat.repeat),
         Container(
           child: SingleChildScrollView(
             child: Column(
