63398c85157e9b3daa76349b083e784b40731686
ibrahim <msri.ibraheem@gmail.com>
2020-03-07 00:49:45 +0200

    Wodking on the homepage

---
+++ lib/AppPages/Home/albumartwidget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../SizeConfig.dart';
+
+class AlbumArtWidget extends StatelessWidget {
+  final String artImageURL;
+  final String albumName;
+  final String artistName;
+
+  AlbumArtWidget({this.artImageURL, this.albumName, this.artistName});
+  SizeConfig _sizeConfig = SizeConfig();
+  @override
+  Widget build(BuildContext context) {
+    _sizeConfig.init(context);
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding:EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal ),
+          child: SizedBox(
+            height: SizeConfig.blockSizeHorizontal*25,
+            width: SizeConfig.blockSizeHorizontal*25,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(14),
+              child: Image.asset("${this.artImageURL}"),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top:5),
+          child: Text(
+            this.albumName,
+            style: TextStyle(
+              fontSize: SizeConfig.blockSizeHorizontal*5.5,
+            ),
+            ),
+        ),
+        Text(
+          this.artistName,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: SizeConfig.blockSizeHorizontal*3,
+          ),
+          ),
+      ],
+    );
+  }
+}
--- lib/AppPages/Home/homepage.dart
+++ lib/AppPages/Home/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:assets_audio_player_example/SizeConfig.dart';
-import 'package:assets_audio_player_example/widgets/albumartwidget.dart';
 import 'package:flutter/material.dart';
+import 'albumartwidget.dart';
 
 class HomePageWidget extends StatefulWidget {
   @override
@@ -13,47 +13,163 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   Widget build(BuildContext context) {
     _sizeConfig.init(context);
 
-    Widget _recentlyPlayedRow = SingleChildScrollView(
+    Widget _musicRow = SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(children: <Widget>[
           AlbumArtWidget(
             albumName: "Album",
-            artImageURL: "art2.jpg",
+            artImageURL: "assets/AlbumImages/art2.jpg",
             artistName: "Shosmo",
           ),
           AlbumArtWidget(
             albumName: "Album",
-            artImageURL: "art2.jpg",
+            artImageURL: "assets/AlbumImages/art3.jpg",
             artistName: "Shosmo",
           ),
           AlbumArtWidget(
             albumName: "Album",
-            artImageURL: "art2.jpg",
+            artImageURL: "assets/AlbumImages/art6.jpg",
             artistName: "Shosmo",
           ),
           AlbumArtWidget(
             albumName: "Album",
-            artImageURL: "art2.jpg",
+            artImageURL: "assets/AlbumImages/art5.jpg",
             artistName: "Shosmo",
           ),
         ]));
-    return Container(
-        child: ListView(
+
+    Widget _recentlyPlayedColumn = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: EdgeInsets.only(
-              top: SizeConfig.blockSizeHorizontal * 2.5,
-              left: SizeConfig.blockSizeHorizontal * 2),
+            top: SizeConfig.blockSizeHorizontal * 2.5,
+            left: SizeConfig.blockSizeHorizontal * 2,
+            bottom: SizeConfig.blockSizeHorizontal * 1.5,
+          ),
           child: Text(
-            "Explore",
+            "Recently Played",
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: SizeConfig.blockSizeHorizontal * 10,
+              fontWeight: FontWeight.w700,
+              fontSize: SizeConfig.blockSizeHorizontal * 5,
             ),
-          ), 
+          ),
         ),
-        _recentlyPlayedRow,
+        Padding(
+          padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizontal),
+          child: _musicRow,
+        ),
       ],
-    ));
+    );
+
+    Widget _recommendedForYouColumn = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(
+            top: SizeConfig.blockSizeHorizontal * 2.5,
+            left: SizeConfig.blockSizeHorizontal * 2,
+            bottom: SizeConfig.blockSizeHorizontal * 1.5,
+          ),
+          child: Text(
+            "Recommended For You",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: SizeConfig.blockSizeHorizontal * 5,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: SizeConfig.blockSizeHorizontal),
+          child: _musicRow,
+        ),
+      ],
+    );
+
+    Widget _top50Widget = Padding(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: 0.85,
+              child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              children: <Widget>[
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(5)),
+                    child: Image.asset("assets/AlbumImages/art6.jpg"),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(5)),
+                    child: Image.asset("assets/AlbumImages/art7.png"),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(5)),
+                    child: Image.asset("assets/AlbumImages/art8.jpg"),
+                  ),
+                ),
+                SizedBox(
+                  width: 50,
+                  height: 200,
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(5)),
+                    child: Image.asset("assets/AlbumImages/art11.jpg"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 240, left: 15),
+            child: Text(
+              "Top 50",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              top: SizeConfig.blockSizeHorizontal * 2.5,
+              left: SizeConfig.blockSizeHorizontal * 2,
+              bottom: SizeConfig.blockSizeHorizontal * 1.5,
+            ),
+            child: Text(
+              "Explore",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: SizeConfig.blockSizeHorizontal * 10,
+              ),
+            ),
+          ),
+          _recentlyPlayedColumn,
+          _recommendedForYouColumn,
+          _top50Widget,
+        ],
+      ),
+    );
   }
 }
