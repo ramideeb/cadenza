02ca697fb31d2071eaf9d50508b21bf04865ae5c
ibrahim <msri.ibraheem@gmail.com>
2020-03-06 22:59:24 +0200

    nth much

--- lib/AppPages/Home/homepage.dart
+++ lib/AppPages/Home/homepage.dart
@@ -1,40 +1,59 @@
 import 'package:assets_audio_player_example/SizeConfig.dart';
+import 'package:assets_audio_player_example/widgets/albumartwidget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePageWidget extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageWidgetState createState() => _HomePageWidgetState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageWidgetState extends State<HomePageWidget> {
+  SizeConfig _sizeConfig = SizeConfig();
   @override
   Widget build(BuildContext context) {
+    _sizeConfig.init(context);
+
+    Widget _recentlyPlayedRow = SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(children: <Widget>[
+          AlbumArtWidget(
+            albumName: "Album",
+            artImageURL: "art2.jpg",
+            artistName: "Shosmo",
+          ),
+          AlbumArtWidget(
+            albumName: "Album",
+            artImageURL: "art2.jpg",
+            artistName: "Shosmo",
+          ),
+          AlbumArtWidget(
+            albumName: "Album",
+            artImageURL: "art2.jpg",
+            artistName: "Shosmo",
+          ),
+          AlbumArtWidget(
+            albumName: "Album",
+            artImageURL: "art2.jpg",
+            artistName: "Shosmo",
+          ),
+        ]));
     return Container(
-      child:ListView(
-        children: <Widget>[
-          Padding(
-            padding: null,
-            child: Text(
-              "Explore",
-              style: TextStyle(
-                fontSize: SizeConfig.blockSizeHorizontal*2,
-              ),
-              ),
-          )
-        ],
-      )
-     
-      
-    );
+        child: ListView(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(
+              top: SizeConfig.blockSizeHorizontal * 2.5,
+              left: SizeConfig.blockSizeHorizontal * 2),
+          child: Text(
+            "Explore",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: SizeConfig.blockSizeHorizontal * 10,
+            ),
+          ), 
+        ),
+        _recentlyPlayedRow,
+      ],
+    ));
   }
 }
-//  SingleChildScrollView(
-//   scrollDirection: Axis.horizontal,
-//   child: Row(
-//    children: <Widget>[
-//      Text('hi'),
-//      Text('hi'),
-//      Text('hi'),
-//    ]
-//   )
-// )
