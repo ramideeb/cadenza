dc69b240419683687498b00edeb9a8b100d30380
YousefAslan <YousefAslan9@gmail.com>
2020-04-21 22:07:49 +0300

    modify FileHandler to work async use .then instead of await add see and delete buttons to JustForTest

--- lib/AppPages/justfortest.dart
+++ lib/AppPages/justfortest.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cadenza/modules/FileHandler.dart';
 import 'package:flutter/material.dart';
 
@@ -17,23 +19,49 @@ class _JustForTestState extends State<JustForTest> {
           RaisedButton(
             child: Text("store file"),
             onPressed: () async {
-              String temp = await fileHandler.storeFile(
+              fileHandler.storeFile(
                   onlineUriString:
                       "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3",
-                  theFileName: "testingSong6.mp3",
-                  theEncryptionKey: "1234");
-              print(temp);
+                  theFileName: "testingSong7.mp3",
+                  theEncryptionKey: "1234").then((String temp)=> print(temp));
             },
           ),
           RaisedButton(
             child: Text("decrypt file"),
             onPressed: () async {
-              String temp = await fileHandler.decryptFile(
+               fileHandler.decryptFile(
                   theEncryptedFilePath:
-                  "/data/user/0/com.cadenza.Cadenza/app_flutter/testingSong6.mp3.aes",
-                  theDecryptedFileName: "testingSong6.mp3",
-                  theEncryptionKey: "1234");
-              print(temp);
+                  "/data/user/0/com.cadenza.Cadenza/app_flutter/testingSong7.mp3.aes",
+                  theDecryptedFileName: "testingSong7.mp3",
+                  theEncryptionKey: "1234").then((String temp)=>print(temp));
+            },
+          ),
+          RaisedButton(
+            child: Text("delete file aes"),
+            onPressed: () async {
+              fileHandler.deleteFile(theFilePath: "/data/user/0/com.cadenza.Cadenza/app_flutter/testingSong7.mp3.aes");
+            },
+          ),
+          RaisedButton(
+            child: Text("delete file cache"),
+            onPressed: () async {
+              fileHandler.deleteFile(theFilePath: "/data/user/0/com.cadenza.Cadenza/cache/testingSong7.mp3");
+            },
+          ),
+          RaisedButton(
+            child: Text("see files cache"),
+            onPressed: () async {
+              Directory("/data/user/0/com.cadenza.Cadenza/cache/").list().forEach((FileSystemEntity f){
+                print(f.toString());
+              });
+            },
+          ),
+          RaisedButton(
+            child: Text("see files system"),
+            onPressed: () async {
+              Directory("/data/user/0/com.cadenza.Cadenza/app_flutter/").list().forEach((FileSystemEntity f){
+                print(f.toString());
+              });
             },
           ),
         ],
