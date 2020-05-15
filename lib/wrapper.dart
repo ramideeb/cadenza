5e83eec98d04a7570166085e75cacfb1123a0dd9
ibrahim <msri.ibraheem@gmail.com>
2020-05-15 17:33:35 +0300

    WORKING ON DELAY

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -13,43 +13,67 @@ import 'modules/queue.dart';
 
 class Wrapper extends StatelessWidget {
   final Auth auth = Auth();
-
+  bool finishedLoadingLibrary = false;
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    Provider.of<Library>(context,listen: false).user = user;
+    Provider.of<Library>(context, listen: false).user = user;
     if (user == null) {
       print(1);
       return Opening();
     } else {
       print(2);
-      Provider.of<Queue>(context,listen: false).initializeQueue();
-      bool songsReady = Provider.of<Library>(context,listen:false).songsReady;
-      if(songsReady)
-        return HomePage();
+      Provider.of<Queue>(context, listen: false).initializeQueue();
+      bool songsReady = Provider.of<Library>(context, listen: false).songsReady;
+      if (songsReady) return HomePage();
       return FutureBuilder<DocumentSnapshot>(
-        future:
-            Firestore.instance.collection('libraries').document(user.uid).get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError)
-            return Scaffold(
-              body: Center(child: Text("Ops")),
-            );
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Scaffold(
-                body: Center(
-              child: CircularProgressIndicator(),
-            ));
-          if (snapshot.connectionState == ConnectionState.done) {
-            if(!snapshot.data.exists)
-            return Text("ops");
-            Provider.of<Library>(context,listen: false)
-                .initialzieSongsFromOnline(snapshot.data);
-            // print(snapshot.data.data['songs']);
-          return HomePage();
-        }
-        }
-      );
+          future: Firestore.instance
+              .collection('libraries')
+              .document(user.uid)
+              .get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError)
+              return Scaffold(
+                body: Center(child: Text("Ops")),
+              );
+            if (snapshot.connectionState == ConnectionState.waiting)
+              return Scaffold(
+                  body: Center(
+                child: CircularProgressIndicator(),
+              ));
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (!snapshot.data.exists)
+                return FutureBuilder(
+                    future: Future.delayed(Duration(seconds: 3)),
+                    builder: (con, delay) {
+                      if (delay.connectionState == ConnectionState.waiting ||
+                          delay.hasError)
+                        return Scaffold(
+                            body: Center(child: CircularProgressIndicator()));
+                      return FutureBuilder<DocumentSnapshot>(
+                          future: Firestore.instance
+                              .collection('libraries')
+                              .document(user.uid)
+                              .get(),
+                          builder: (con, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting||snapshot.hasError)
+                              return Scaffold(
+                                body:
+                                    Center(child: CircularProgressIndicator()),
+                              );
+
+                            Provider.of<Library>(context, listen: false)
+                                .initialzieSongsFromOnline(snapshot.data);
+                            // print(snapshot.data.data['songs']);
+                            return HomePage();
+                          });
+                    });
+              Provider.of<Library>(context, listen: false)
+                  .initialzieSongsFromOnline(snapshot.data);
+              return HomePage();
+            }
+          });
     }
   }
 }
